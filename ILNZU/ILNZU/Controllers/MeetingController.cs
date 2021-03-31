7396df83a54bbe9bb4f130c54a8685de8a022cbc
slavkokp <spkolodiy@gmail.com>
2021-03-31 17:14:14 +0300

    added signalR and chat page with hub

---
+++ ILNZU/ILNZU/Controllers/MeetingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ILNZU.Controllers
+{
+    public class MeetingController : Controller
+    {
+        [Authorize]
+        public IActionResult Room(int? id)
+        {
+            return View(id);
+        }
+    }
+}
