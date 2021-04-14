6ca4c6b8c478cc41c2d2a6d2d26cfbe41e820156
yaremaStiahar <zapasnyy@gmail.com>
2021-04-14 15:22:15 +0300

    TEEEEsty

--- ILNZU/TEEEEEsts/UnitTest1.cs
+++ ILNZU/TEEEEEsts/UnitTest1.cs
@@ -1,14 +1,28 @@
-using Microsoft.EntityFrameworkCore;
-using System;
-using Xunit;
-
 namespace TEEEEEsts
 {
-    public class UnitTest1
+    using System;
+    using BLL;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public void PasswordHashTheSameTest()
         {
+            string password = "555";
+            string saltedPassword = password + PasswordHash.GetSalt();
+            string hash = PasswordHash.HashPassword(saltedPassword);
+            Assert.Equal(hash, PasswordHash.HashPassword(saltedPassword));
         }
+
+        [Fact]
+        public void PasswordHashTest()
+        {
+            string password = "555";
+            string hash = PasswordHash.HashPassword(password);
+            Assert.Equal(hash, "de21c670ae7c3f6f3f1f37029303c9");
+        }
+
     }
 }
